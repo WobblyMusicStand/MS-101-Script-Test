c3c3f3ff5942a387ed48321bb7ab696f0a9c3804
WobblyMusicStand <WobblyMusicStand@gmail.com>
2021-12-01 19:18:37 -0500

    Added sharepoint script v1, requires testing on remote environment.

---
+++ MS-101_Sharepoint.ps1
@@ -0,0 +1,26 @@
+﻿Param(
+    [Parameter(Mandatory=$True)]
+    [string]$TenantName,
+    [Parameter(Mandatory=$True)]
+    [string]$TenantID,
+    [Parameter(Mandatory=$True)]
+    [string]$Password
+)
+
+Connect-AzureAD
+Get-AzureADDirectorySettingTemplate
+$TemplateId = (Get-AzureADDirectorySettingTemplate | where { $_.DisplayName -eq "Group.Unified" }).Id
+$Template = Get-AzureADDirectorySettingTemplate | where -Property Id -Value $TemplateId -EQ
+$Setting = $Template.CreateDirectorySetting()
+$setting["EnableMIPLabels"] = "True"
+
+New-AzureADDirectorySetting -DirectorySetting $Setting
+Connect-SPOService -Url "https://$TenantID" + "-admin.sharepoint.com" -Credential "Office365Admin@$TenantName"
+
+$Password 
+
+
+Set-SPOTenant -EnableAIPIntegration $true 
+Install-Module ExchangeOnlineManagement.
+Connect-IPPSSession -UserPrincipalName "Office365Admin@$TenantName"
+Execute-AzureAdLabelSyn
